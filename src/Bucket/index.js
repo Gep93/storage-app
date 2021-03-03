4f582d2afa62d47400bf2ac7c98df721ed7b99c5
Gal Erzen Pajic <gal.erzen.pajic@gmail.com>
2021-03-03 22:54:07 +0100

    add(component): Bucket

---
+++ src/Bucket/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+
+class Bucket extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="Bucket p-3">
+        <h1>MyBucket</h1>
+      </div>
+    );
+  }
+}
+
+export default Bucket;
