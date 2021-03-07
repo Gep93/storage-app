c51c9332bc6f79839292f339d76dde9f7ca94e1b
Gal Erzen Pajic <gal.erzen.pajic@gmail.com>
2021-03-07 17:01:48 +0100

    add: padding to App-banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import BucketList from "./BucketList/index";
-import "./App.css";
 import Bucket from "./Bucket/index";
 import { Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-banner">
+      <div className="App-banner py-3 px-5">
         <Link to="/" className="App-link">
           Secure cloud storage
         </Link>
